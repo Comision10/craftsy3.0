bf8505ee88ff0bad7bf8845bafa68dde8e6f8d6b
Eric Mena <53233372+EricM76@users.noreply.github.com>
2021-10-19 10:06:59 -0300

    add migrations&models

---
+++ src/database/config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: "Marvel1908!",
+    database: "craftsy3.0_dbC10",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    port : 3306
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
